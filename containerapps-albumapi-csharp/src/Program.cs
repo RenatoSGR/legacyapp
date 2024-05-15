dfdfcbb0dbbafdb0f44d5f77e1f3f56022acd3b7
RenatoSGR <renribeiro@microsoft.com>
2024-05-15 21:19:18 +0100

    refactor: Update albadding albuns to catalog

--- containerapps-albumapi-csharp/src/Program.cs
+++ containerapps-albumapi-csharp/src/Program.cs
@@ -42,13 +42,18 @@ record Album(int Id, string Title, string Artist, double Price, string Image_url
 {
      public static List<Album> GetAll(){
          var albums = new List<Album>(){
-            new Album(1, "You, Me and an App Id", "Daprize", 10.99, "https://aka.ms/albums-daprlogo"),
-            new Album(2, "Seven Revision Army", "The Blue-Green Stripes", 13.99, "https://aka.ms/albums-containerappslogo"),
-            new Album(3, "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
-            new Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
-            new Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
-            new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
+            new Album(1, "Fairly Suggestion", "Twenty One CoPilots", 19.00, "https://upload.wikimedia.org/wikipedia/commons/8/8a/GitHub_Copilot_logo.svg"),
+            new Album(2, "You, Me and an App Id", "Daprize", 10.99, "https://aka.ms/albums-daprlogo"),
+            new Album(3, "Seven Revision Army", "The Blue-Green Stripes", 13.99, "https://aka.ms/albums-containerappslogo"),
+            new Album(4, "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
+            new Album(5, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
+            new Album(6, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
+            new Album(7, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
+            
+            
          };
+
+        
 
         return albums; 
      }
